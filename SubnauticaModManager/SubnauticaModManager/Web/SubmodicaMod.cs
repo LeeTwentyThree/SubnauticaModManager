e8592bd79010457d3e1bdc5f0ef1094ddb373f39
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-01-01 20:00:22 -0500

    load webps properly

--- SubnauticaModManager/SubnauticaModManager/Web/SubmodicaMod.cs
+++ SubnauticaModManager/SubnauticaModManager/Web/SubmodicaMod.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using UnityEngine.Networking;
 using Newtonsoft.Json;
+using SubnauticaModManager.Web.Images;
+using System.Drawing;
 
 namespace SubnauticaModManager.Web;
 
@@ -78,9 +80,26 @@ public class SubmodicaMod
     {
         if (!string.IsNullOrEmpty(profile_image))
         {
-            using (var request = UnityWebRequestTexture.GetTexture(profile_image, false))
+            using (var request = UnityWebRequest.Get(profile_image))
             {
-                yield return new WaitForSeconds(0.05f);
+                yield return request.SendWebRequest();
+                var data = request.downloadHandler.data;
+                if (data != null)
+                {
+                    WebP image = new WebP();
+                    var bitmap = image.Decode(data, new WebPDecoderOptions(), System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+                    if (bitmap != null && bitmap.Width > 0 && bitmap.Height > 0)
+                    {
+                        Texture2D tex = new Texture2D(2, 2);
+                        using (MemoryStream ms = new MemoryStream())
+                        {
+                            bitmap.Save(ms, bitmap.RawFormat);
+                            tex.LoadImage(ms.ToArray());
+                        }
+                        tex.Apply();
+                        ModImageSprite = Sprite.Create(tex, new Rect(0.0f, 0.0f, tex.width, tex.height), new Vector2(0.5f, 0.5f));
+                    }
+                }
             }
         }
     }
