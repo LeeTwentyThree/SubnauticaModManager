b92e3ffcdc529ad156dea71a5f6edd2982849ba5
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-01-03 06:17:44 -0500

    ModManagerFileArranger.API

---
+++ SubnauticaModManager/ModManagerFileArranger/API.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+
+namespace ModManagerFileArranger;
+
+public static class API
+{
+    public static void Run(string exePath, string instructionsPath)
+    {
+        var process = new Process();
+        process.StartInfo.FileName = exePath;
+        process.StartInfo.Arguments = "\"" + instructionsPath + "\"";
+        process.StartInfo.CreateNoWindow = true;
+        process.StartInfo.UseShellExecute = false;
+        process.Start();
+    }
+}
