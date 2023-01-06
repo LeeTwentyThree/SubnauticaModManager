e5e4d6e4e37f660be3145558a3eca59e05d6eda5
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-01-05 22:07:45 -0500

    NoPluginException

---
+++ SubnauticaModManager/SubnauticaModManager/Files/Exceptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SubnauticaModManager.Files;
+
+public class NoPluginException : Exception
+{
+    public NoPluginException()
+    {
+    }
+
+    public override string Message => "The specified mod folder does not contain a valid plugin file!";
+}
