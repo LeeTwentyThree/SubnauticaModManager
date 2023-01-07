ab3e46e98f87afcdcca9478b1a1b6281973ad2f0
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-01-07 12:16:58 -0500

    Create PluginDependency.cs

---
+++ SubnauticaModManager/SubnauticaModManager/Files/PluginDependency.cs
@@ -0,0 +1,13 @@
+﻿using DependencyFlags = BepInEx.BepInDependency.DependencyFlags;
+
+namespace SubnauticaModManager.Files;
+
+public class PluginDependency
+{
+    public string guid;
+    public DependencyFlags flags;
+
+    public bool IsSoft => flags.HasFlag(DependencyFlags.SoftDependency);
+
+    public bool IsHard => flags.HasFlag(DependencyFlags.HardDependency);
+}
