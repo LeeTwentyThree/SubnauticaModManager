54271ed142e3f69bb6d7a998bd2705fa9753ea85
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-01-08 21:00:29 -0500

    GetChecksum

---
+++ SubnauticaModManager/SubnauticaModManager/Web/SubmodicaGUIDs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace SubnauticaModManager.Web;
+
+internal static class SubmodicaGUIDs
+{
+    public static string GetChecksum(string filename)
+    {
+        using (var md5 = MD5.Create())
+        {
+            using (var stream = File.OpenRead(filename))
+            {
+                var hash = md5.ComputeHash(stream);
+                return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+            }
+        }
+    }
+}
