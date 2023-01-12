c95f9119a192b201e78666809ef82af75ce91c1f
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-01-11 19:17:01 -0500

    Update KnownPlugins.cs

--- SubnauticaModManager/SubnauticaModManager/Files/KnownPlugins.cs
+++ SubnauticaModManager/SubnauticaModManager/Files/KnownPlugins.cs
@@ -3,4 +3,21 @@
 internal class KnownPlugins
 {
     public static List<PluginData> list = new List<PluginData>();
+
+    public static PluginData GetByGUID(string guid)
+    {
+        if (list == null) return null;
+        foreach (var plugin in list)
+        {
+            if (plugin.GUID != null && plugin.GUID.Equals(guid)) return plugin;
+        }
+        return null;
+    }
+
+    public static bool GetUpdateAvailable(string guid, System.Version latest)
+    {
+        var plugin = GetByGUID(guid);
+        if (plugin == null || !plugin.IsValid) return false;
+        return latest > plugin.Version;
+    }
 }
