3b3bef3769650e255fe76be0ce0e8fe5c5139973
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2023-01-16 21:45:39 -0500

    logger for mod installation process

--- SubnauticaModManager/ModManagerFileArranger/Program.cs
+++ SubnauticaModManager/ModManagerFileArranger/Program.cs
@@ -1,17 +1,20 @@
 ﻿using FileArranger;
+using System.Text;
 using System.Diagnostics;
 using ModManagerFileArranger;
 
 internal static class Application
 {
+    private static StringBuilder logOutput = new StringBuilder();
+
     public static void Main(string[] args) // arg 0 : instructions JSON path, arg 1: game id
     {
-        Console.WriteLine("\n[ Subnautica Mod Manager for BepInEx File Arranger ]\n");
-        Console.WriteLine("- This application was designed for automatic use, but can be utilized manually for testing purposes.\n");
+        Log("\n[ Subnautica Mod Manager for BepInEx File Arranger ]\n");
+        Log("- This application was designed for automatic use, but can be utilized manually for testing purposes.\n");
 
         if (args == null || args.Length < 1 || string.IsNullOrEmpty(args[0]))
         {
-            Console.WriteLine("Invalid arguments.\n" +
+            Log("Invalid arguments.\n" +
                 "Argument 0 should be a valid file path for the instructions JSON file. This window will automatically close on input.");
             Console.ReadKey();
             return;
@@ -19,38 +22,62 @@ internal static class Application
 
         var instructionsPath = args[0];
 
-        Console.WriteLine($"Instructions JSON path: '{instructionsPath}'.");
+        Log($"Instructions JSON path: '{instructionsPath}'.");
 
         if (!File.Exists(instructionsPath))
         {
-            Console.WriteLine("File not found! Closing.");
+            Log("File not found! Closing.");
+            SaveLogFile(instructionsPath);
             return;
         }
 
-        Console.WriteLine("Path appears to be valid.");
+        Log("Path appears to be valid.");
 
-        if (GetIsSubnauticaRunning()) Console.WriteLine("Game is still loaded! Waiting for it to close.");
+        if (GetIsSubnauticaRunning()) Log("Game is still loaded! Waiting for it to close.");
         while (GetIsSubnauticaRunning())
         {
             Thread.Sleep(10);
         }
         Thread.Sleep(200);
 
-        Console.WriteLine("Game is not active. Ready to modify files.");
+        Log("Game is not active. Ready to modify files.");
 
         var instructionSet = new InstructionSet(instructionsPath);
 
         var results = instructionSet.ExecuteInstructions();
 
-        Console.WriteLine($"Executed instructions. Outputting results:");
+        Log($"Executed instructions. Outputting results:");
         for (int i = 0; i < results.Length; i++)
         {
-            Console.WriteLine(i + Environment.NewLine + results[i]);
+            Log(i + Environment.NewLine + results[i]);
         }
 
         if (TryGetGameID(args, out var platform))
         {
             SNLoader.LoadGame(platform);
+        }
+
+        Log("Success!");
+
+        SaveLogFile(instructionsPath);
+    }
+
+    private static void SaveLogFile(string instructionsPath)
+    {
+        try
+        {
+            var logOutputPath = Path.Combine(Path.GetDirectoryName(instructionsPath), "installation-log.txt");
+            File.WriteAllText(logOutputPath, logOutput.ToString());
+        }
+        catch { }
+    }
+
+    private static void Log(string msg = "")
+    {
+        if (msg != null)
+        {
+            Console.WriteLine(msg);
+            logOutput.AppendLine(msg);
         }
     }
 
@@ -66,10 +93,12 @@ internal static class Application
             if (args[1] != null && int.TryParse(args[1], out int result))
             {
                 platform = (SNPlatform)result;
+                Log($"Launching game on platform {platform}.");
                 return platform != SNPlatform.Unknown;
             }
         }
         platform = SNPlatform.Unknown;
+        Log("Unable to determine platform. Game will not restart.");
         return false;
     }
 }
